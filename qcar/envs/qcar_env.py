8375c9eb1b475421606a8d475987ad83f5a0aaab
Henry Chen <henrycrh@gmail.com>
2019-03-03 06:22:20 -0800

    commit

--- qcar/envs/qcar_env.py
+++ qcar/envs/qcar_env.py
@@ -3,5 +3,96 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 class QcarEnv(gym.Env):
-    def __init(self):
-        pass
+    metadata = {
+        "render.modes": ["human"],
+    }
+
+    ACTION = ["N", "S", "E", "W"]
+
+    def __init__(self, maze_file=None, maze_size=None, mode=None):
+
+        self.viewer = None
+
+        if maze_file:
+            self.maze_view = MazeView2D(maze_name="OpenAI Gym - Maze (%s)" % maze_file,
+                                        maze_file_path=maze_file,
+                                        screen_size=(640, 640))
+        elif maze_size:
+            if mode == "plus":
+                has_loops = True
+                num_portals = int(round(min(maze_size)/3))
+            else:
+                has_loops = False
+                num_portals = 0
+
+            self.maze_view = MazeView2D(maze_name="OpenAI Gym - Maze (%d x %d)" % maze_size,
+                                        maze_size=maze_size, screen_size=(640, 640),
+                                        has_loops=has_loops, num_portals=num_portals)
+        else:
+            raise AttributeError("One must supply either a maze_file path (str) or the maze_size (tuple of length 2)")
+
+        self.maze_size = self.maze_view.maze_size
+
+        # forward or backward in each dimension
+        self.action_space = spaces.Discrete(2*len(self.maze_size))
+
+        # observation is the x, y coordinate of the grid
+        low = np.zeros(len(self.maze_size), dtype=int)
+        high =  np.array(self.maze_size, dtype=int) - np.ones(len(self.maze_size), dtype=int)
+        self.observation_space = spaces.Box(low, high)
+
+        # initial condition
+        self.state = None
+        self.steps_beyond_done = None
+
+        # Simulation related variables.
+        self._seed()
+        self.reset()
+
+        # Just need to initialize the relevant attributes
+        self._configure()
+
+    def __del__(self):
+        self.maze_view.quit_game()
+
+    def _configure(self, display=None):
+        self.display = display
+
+    def _seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def _step(self, action):
+        if isinstance(action, int):
+            self.maze_view.move_robot(self.ACTION[action])
+        else:
+            self.maze_view.move_robot(action)
+
+        if np.array_equal(self.maze_view.robot, self.maze_view.goal):
+            reward = 1
+            done = True
+        else:
+            reward = -0.1/(self.maze_size[0]*self.maze_size[1])
+            done = False
+
+        self.state = self.maze_view.robot
+
+        info = {}
+
+        return self.state, reward, done, info
+
+    def _reset(self):
+        self.maze_view.reset_robot()
+        self.state = np.zeros(2)
+        self.steps_beyond_done = None
+        self.done = False
+        return self.state
+
+    def is_game_over(self):
+        return self.maze_view.game_over
+
+    def _render(self, mode="human", close=False):
+        if close:
+            self.maze_view.quit_game()
+
+        return self.maze_view.update(mode)
